cd54c4c46f80f6ac30a328ac59341511a618c3a6
Matthew Pratt <matthew.pratt13@gmail.com>
2026-09-10 14:56:12 +0200

    add AccountOutput model

--- src/account.rs
+++ src/account.rs
@@ -1,8 +1,20 @@
 use rust_decimal::Decimal;
+use serde::Serialize;
+
+use crate::types::ClientId;
 
 #[derive(Debug, Default)]
 pub struct Account {
     available: Decimal,
     held: Decimal,
+    locked: bool,
+}
+
+#[derive(Debug, Serialize)]
+pub struct AccountOutput {
+    client: ClientId,
+    available: Decimal,
+    held: Decimal,
+    total: Decimal,
     locked: bool,
 }
